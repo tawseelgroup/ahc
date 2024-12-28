90b9a1815f7b1452bd47d8303247f9aee761f819
tawseelgroup <tawseelgroup943@gmail.com>
2024-12-28 17:04:33 +0300

    seconds update

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path('hr/', views.hr, name='hr'),
+    path('payroll/', views.payroll, name='payroll'),
+    path('assignment/', views.assignment, name='assignment'),
+    path('jobs/', views.jobs, name='jobs'),
+    path('recruitment/', views.recruitment, name='recruitment'),
+    path('usersmanagement/', views.usersmanagement, name='usersmanagement'),
+
+    path('newassignment', views.newassignment, name='newassignment'),
 ]
